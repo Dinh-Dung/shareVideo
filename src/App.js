28fc1b2a4452f241bbf651906f71182dc76b3bad
Dung-Dinh <dungvanbmt.01@gmail.com>
2023-03-07 21:34:47 +0700

    Router dom config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout/DefaultLayout/DefaultLayout';
 function App() {
     return (
-        <div className="App">
-            <h2>React App</h2>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/components/Layout/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// import Header from '~/components/Layout/components/Header';
+// import Slidebar from '~/components/Layout/components/Slidebar';
+export const DefaultLayout = () => {
+    return <div>DefaultLayout</div>;
+};
